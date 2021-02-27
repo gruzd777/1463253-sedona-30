d63f4cf6c95d1440af72c42a979b247b0f5bce27
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2021-02-27 17:21:07 +0300

    Правки и исправления

--- js/open-form.js
+++ js/open-form.js
@@ -1,6 +1,9 @@
 let toggleForm = document.querySelector('.search__form');
 let toggleTitle = document.querySelector('.search__link');
+let focus = toggleForm.querySelector('#date-first');
 
-toggleTitle.addEventListener('click', function () {
+toggleTitle.addEventListener('click', function (evt) {
+    evt.preventDefault();
     toggleForm.classList.toggle('show');
+    focus.focus();
 });
