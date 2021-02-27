298b7ba9f5a793e2fa049afae4e86f255b274fba
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2021-02-27 15:32:40 +0300

    Добавил всплывание формы

--- js/toggle-form.js
+++ js/toggle-form.js
@@ -1,8 +1,6 @@
-"use strict";
-
 let toggleForm = document.querySelector('.search__form');
 let toggleTitle = document.querySelector('.search__link');
 
-toogleTitle.onclick = function() {
-    toggleForm.classList.add('show');
-};
+toggleTitle.addEventListener('click', function () {
+    toggleForm.classList.toggle('show');
+});
