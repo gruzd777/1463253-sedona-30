47cd506c5dcb97551ed2de9308a959665e8f93e9
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2021-03-05 17:54:43 +0300

    Завершена главная страница

--- js/open-form.js
+++ js/open-form.js
@@ -1,19 +1,38 @@
 const toggleForm = document.querySelector('.search__form');
 const toggleTitle = document.querySelector('.search__link');
 const focus = toggleForm.querySelector('#date-first');
+const formInput = toggleForm.querySelector('.search__input');
+const formButton = toggleForm.querySelector('.search__button');
+
+toggleForm.classList.add('hide');
 
 toggleTitle.addEventListener('click', function (evt) {
     evt.preventDefault();
-    toggleForm.classList.toggle('show');
+    toggleForm.classList.toggle('hide');
     focus.focus();
+    if (toggleForm.classList.contains('error')) {
+        toggleForm.classList.remove('error');
+    };
 });
 
 window.addEventListener('keydown', function (event) { 
     if (event.code === 'Escape') { 
-        if (toggleForm.classList.contains('show')) {
+        toggleForm.classList.remove('error');
+
+        if (!toggleForm.classList.contains('hide')) {
             event.preventDefault();
-            toggleForm.classList.remove('show');
+            toggleForm.classList.add('hide');
         };
     };
 });
+
+toggleForm.addEventListener('submit', function (evt) {
+    if (!formInput.value) {
+        evt.preventDefault();
+        toggleForm.classList.remove('error');
+        toggleForm.offsetWidth = toggleForm.offsetWidth;
+        toggleForm.classList.add('error');
+    };
+});
+
 
