675d3c21458ff62055be90968fe1e0f70677683d
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2021-03-10 19:10:02 +0300

    Завершено

--- js/open-form.js
+++ js/open-form.js
@@ -1,19 +1,26 @@
 const toggleForm = document.querySelector('.search__form');
 const toggleTitle = document.querySelector('.search__link');
-const focus = toggleForm.querySelector('#date-first');
-const formInput = toggleForm.querySelector('.search__input');
-const formButton = toggleForm.querySelector('.search__button');
+const focus = document.querySelector('#date-first');
+const formInput = document.querySelector('.search__input');
+const formButton = document.querySelector('.search__button');
+const resultsLinks = document.querySelectorAll('.results__link');
+const directionLinks = document.querySelectorAll('.direction-link');
 
-toggleForm.classList.add('hide');
+if (toggleForm) {
+    toggleForm.classList.add('hide');    
+}
 
-toggleTitle.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    toggleForm.classList.toggle('hide');
-    focus.focus();
-    if (toggleForm.classList.contains('error')) {
-        toggleForm.classList.remove('error');
-    };
-});
+if (toggleTitle) {
+    toggleTitle.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        toggleForm.classList.toggle('hide');
+        focus.focus();
+        if (toggleForm.classList.contains('error')) {
+            toggleForm.classList.remove('error');
+        };
+    });
+}
+
 
 window.addEventListener('keydown', function (event) { 
     if (event.code === 'Escape') { 
@@ -26,13 +33,33 @@ window.addEventListener('keydown', function (event) {
     };
 });
 
-toggleForm.addEventListener('submit', function (evt) {
-    if (!formInput.value) {
-        evt.preventDefault();
-        toggleForm.classList.remove('error');
-        toggleForm.offsetWidth = toggleForm.offsetWidth;
-        toggleForm.classList.add('error');
-    };
-});
+if (toggleForm) {
+    toggleForm.addEventListener('submit', function (evt) {
+        if (!formInput.value) {
+            evt.preventDefault();
+            toggleForm.classList.remove('error');
+            toggleForm.offsetWidth = toggleForm.offsetWidth;
+            toggleForm.classList.add('error');
+        };
+    });
+}
 
+if (resultsLinks) {
+    resultsLinks.forEach(function(elem, i){
+        elem.addEventListener('click', function(event){
+            event.preventDefault();
+            document.querySelector('.results__link--active').classList.remove('results__link--active');
+            this.classList.add('results__link--active');
+        });
+    });
+}
 
+if (directionLinks) {
+    directionLinks.forEach(function(directionLink, i){
+        directionLink.addEventListener('click', function(event){
+            event.preventDefault();
+            document.querySelector('.down__active').classList.remove('down__active');
+            this.classList.add('down__active');
+        });
+    });
+}
